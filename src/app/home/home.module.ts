11e0868c947628f719eb3d768e182389d79e5db1
abhay <abhayviswanath.1483@gmail.com>
2020-08-02 22:29:58 +0530

    how to description added

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,11 +5,12 @@ import { HeaderComponent } from './header/header.component';
 import { FeaturesComponent } from './features/features.component';
 import { FavouritesComponent } from './favourites/favourites.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { HowToComponent } from './how-to/how-to.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, FeaturesComponent, FavouritesComponent, HomePageComponent],
+  declarations: [HeaderComponent, FeaturesComponent, FavouritesComponent, HomePageComponent, HowToComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
