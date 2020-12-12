b5e93b51dbbdbe1a060f9c1d8702eb3a02b38685
abhay <abhayviswanath.1483@gmail.com>
2020-12-12 12:11:24 +0530

    testimonial section added

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,11 +7,12 @@ import { FavouritesComponent } from './favourites/favourites.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { HowToComponent } from './how-to/how-to.component';
 import { CitiesComponent } from './cities/cities.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, FeaturesComponent, FavouritesComponent, HomePageComponent, HowToComponent, CitiesComponent],
+  declarations: [HeaderComponent, FeaturesComponent, FavouritesComponent, HomePageComponent, HowToComponent, CitiesComponent, TestimonialComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
